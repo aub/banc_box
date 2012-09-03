077ae7d58218796591bb13bd1ba6ca11d9a277cb
Aubrey Holland <aubrey@brewster.com>
2012-09-02 22:28:04 -0400

    require correctly

--- lib/banc_box.rb
+++ lib/banc_box.rb
@@ -2,7 +2,7 @@ require 'faraday'
 require 'faraday_middleware'
 
 %w(config connection).each do |file|
-  require File.join(File.dirname(__FILE__), 'bancbox', file)
+  require File.join(File.dirname(__FILE__), 'banc_box', file)
 end
 
 module BancBox
